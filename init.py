7f07472e77923d6839ec207e55e87222c547d035
Jay Mehta <78312356+BitsJayMehta173@users.noreply.github.com>
2024-08-26 10:37:57 +0530

    completed the initial question spooter according to pdf

---
+++ init.py
@@ -0,0 +1,31 @@
+import PyPDF2
+
+# Open the PDF file
+file_path = 'model.pdf'
+pages=[]
+
+with open(file_path, 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    
+    extracted_text = ""
+
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        extracted_text += page.extract_text()
+        pages.append(page.extract_text())
+    
+    questions=[]
+
+    text=""
+    for page in pages:
+        for i in range(len(page)-1):
+            if page[i]=='\n' and page[i+1].isdigit():
+                print(text)
+                questions.append(text)
+                print("end")
+                text=""
+            else:
+                text+=page[i]
+
+
+
